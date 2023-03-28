09ccd6a3db5ac3300d7741800bb11c384af085b0
Erin Keller <erinkeller@Erins-MacBook-Pro-2.local>
2023-03-28 11:23:17 -0400

    Added generateSquare function

--- utils/generateSvg.js
+++ utils/generateSvg.js
@@ -1,5 +1,6 @@
+let hexRegex = /^[a-fA-F0-9]{6}$|^[a-fA-F0-9]{3}$/;
+
 function generateCircle (title,titleColor,shapeColor) {
-    let hexRegex = /^[a-fA-F0-9]{6}$|^[a-fA-F0-9]{3}$/;
     if (hexRegex.test(titleColor)) {
         titleColor = "#" + titleColor;
     }
@@ -15,11 +16,27 @@ function generateCircle (title,titleColor,shapeColor) {
     return circleSvg;
 }
 
+function generateSquare (title,titleColor,shapeColor) {
+    if (hexRegex.test(titleColor)) {
+        titleColor = "#" + titleColor;
+    }
+    if (hexRegex.test(shapeColor)) {
+        shapeColor = "#" + shapeColor;
+    }
+    const squareSvg = `
+        <svg viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg">
+            <rect x="50" y="50" width="200" height="100" fill="${shapeColor}"/>
+            <text x="150" y="100" text-anchor="middle" dominant-baseline="central" font-size="24" fill="${titleColor}">${title}</text>
+        </svg>
+    `;
+    return squareSvg;
+}
+
 const generateSvg = data => {
     if (data.shape === 'circle') {
         return generateCircle(data.title,data.titleColor,data.shapeColor);
     } else if (data.shape === 'square') {
-        return generateCircle(data.title,data.titleColor,data.shapeColor);
+        return generateSquare(data.title,data.titleColor,data.shapeColor);
     } else if (data.shape === 'triangle') {
         return generateCircle(data.title,data.titleColor,data.shapeColor);
     }
